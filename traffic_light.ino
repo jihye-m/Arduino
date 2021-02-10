9c1180df16287a9682722d03384f83d7f9f9f8aa
jihye-m <68888824+jihye-m@users.noreply.github.com>
2021-02-10 23:27:23 +0900

    Add files via upload

---
+++ traffic_light.ino
@@ -0,0 +1,21 @@
+int REDpin = 6;
+int YELLOWpin = 5;
+int GREENpin = 4;
+
+void setup() {
+  pinMode(REDpin, OUTPUT);
+  pinMode(YELLOWpin,OUTPUT);
+  pinMode(GREENpin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(REDpin, HIGH);
+  delay(5000);
+  digitalWrite(REDpin, LOW);
+  digitalWrite(YELLOWpin, HIGH);
+  delay(5000);
+  digitalWrite(YELLOWpin, LOW);
+  digitalWrite(GREENpin, HIGH);
+  delay(5000);
+  digitalWrite(GREENpin, LOW);
+}
